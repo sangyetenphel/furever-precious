65767a9d66628da09072b8a59b95fdf7d7fa1367
sangyetenphel <sangyetenphel@gmail.com>
2021-07-04 21:08:55 -0400

    Changes secret key settings

--- furever_precious/settings.py
+++ furever_precious/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-52-g+ao3*or)j6!!*td1(er-3d=-51j0f_yx27lc$8a3nw7w3v')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
