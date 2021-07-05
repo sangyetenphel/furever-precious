c3473eb4ebd340ebf041534e771d50e5aa2fe16c
sangyetenphel <sangyetenphel@gmail.com>
2021-07-05 12:40:59 -0400

    Updated requirements.txt

--- store/models.py
+++ store/models.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.safestring import mark_safe
-from PIL import Image
 from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
@@ -30,16 +29,6 @@ class Product(models.Model):
 
     def  __str__(self):
         return self.name
-
-    # def save(self):
-    #     super().save()
-         
-    #     img = Image.open(self.image.path)
-
-    #     if img.height > 300 or img.width > 300:
-    #         output_size = (300, 300)
-    #         img.thumbnail(output_size)
-    #         img.save(self.image.path)
 
     def image_tag(self):
         # mark_safe as an html for output in the admin page
